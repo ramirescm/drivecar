29228ed495cd7d44a950063f2a8c87274f0097cf
Ramires <ramirescm@gmail.com>
2024-07-10 21:19:53 -0300

    chore: connection with bucket

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/ramirescm/drivecar
 
 go 1.21.0
 
-require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+require github.com/aws/aws-sdk-go v1.54.18
+
+require (
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+)
---
+++ internal/bucket/bucket.go
@@ -0,0 +1,52 @@
+package bucket
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+)
+
+const (
+	AwsProvider BucketType = iota
+)
+
+type BucketType int
+
+func New(bt BucketType, cfg any) (b *Bucket, err error) {
+	rt := reflect.TypeOf(cfg)
+
+	switch bt {
+	case AwsProvider:
+		if rt.Name() != "AwsConfig" {
+			return nil, fmt.Errorf("config needs to be a typeof RabbitMQConfig")
+		}
+
+		b.p = newAwsSession(cfg.(AwsConfig))
+	default:
+		return nil, fmt.Errorf("type not implemented")
+	}
+	return
+}
+
+type BucketInterface interface {
+	Upload(io.Reader, string) error
+	Download(string, string) (*os.File, error)
+	Delete(string) error
+}
+
+type Bucket struct {
+	p BucketInterface
+}
+
+func (b *Bucket) Upload(file io.Reader, key string) error {
+	return b.p.Upload(file, key)
+}
+
+func (b *Bucket) Download(src, dst string) (file *os.File, err error) {
+	return b.p.Download(src, dst)
+}
+
+func (b *Bucket) Delete(key string) error {
+	return b.p.Delete(key)
+}
