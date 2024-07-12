b60e18c370645b2a6c226a97d1c76c2dc9267094
Ramires <ramirescm@gmail.com>
2024-07-12 20:56:20 -0300

    chore: add delete files from folders

--- internal/folders/delete.go
+++ internal/folders/delete.go
@@ -7,10 +7,18 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/ramirescm/drivecar/internal/files"
 )
 
 func (h *handler) Delete(rw http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = deleteFiles(h.db, int64(id))
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
@@ -23,6 +31,33 @@ func (h *handler) Delete(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	rw.Header().Add("Content-Type", "application/json")
+}
+
+func deleteFiles(db *sql.DB, folderID int64) error {
+	f, err := files.GetAll(db, folderID)
+	if err != nil {
+		return err
+	}
+
+	removedFiles := make([]file.File, 0, len(f))
+	for _, file := range f {
+		file.Deleted = true
+		err = files.Update(db, file.ID, &file)
+		if err != nil {
+			break
+		}
+
+		removedFiles = append(removedFiles, file)
+	}
+
+	if len(f) != len(removedFiles) {
+		for _, file := range removedFiles {
+			file.Deleted = false
+			files.Update(db, file.ID, &file)
+		}
+	}
+
+	return nil
 }
 
 func Delete(db *sql.DB, id int64) error {
