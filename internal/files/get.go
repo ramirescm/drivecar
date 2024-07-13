5e0024fe79329924c78c6472f92bfc822f3dcbdf
Ramires <ramirescm@gmail.com>
2024-07-13 09:19:14 -0300

    chore: update file name

---
+++ internal/files/get.go
@@ -0,0 +1,17 @@
+package files
+
+import "database/sql"
+
+func Get(db *sql.DB, id int64) (*File, error) {
+	stmt := `select * from "files" where id = $1`
+	row := db.QueryRow(stmt, id)
+
+	var f File
+	err := row.Scan(&f.ID, &f.FolderID, &f.OwnerID, &f.Name, &f.Path, &f.CreatedAt, &f.ModifiedAt, &f.Deleted)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &f, nil
+}
