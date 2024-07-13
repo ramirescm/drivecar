fd98f62cc8cf4cca5a78b6d4f3bf12e458ee8a02
Ramires <ramirescm@gmail.com>
2024-07-12 21:40:15 -0300

    chore: get folders and subfolders

--- internal/folders/get.go
+++ internal/folders/get.go
@@ -16,18 +16,26 @@ func (h *handler) Get(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	u, err := Get(h.db, int64(id))
+	f, err := GetFolder(h.db, int64(id))
 	if err != nil {
 		// todo validate if record is not found
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
+	c, err := GetFolderContent(h.db, int64(id))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fc := FolderContent{Folder: *f, Content: c}
+
 	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(u)
+	json.NewEncoder(rw).Encode(fc)
 }
 
-func Get(db *sql.DB, id int64) (*Folder, error) {
+func GetFolder(db *sql.DB, id int64) (*Folder, error) {
 	stmt := `select * from "folders" where id = $1`
 	row := db.QueryRow(stmt, id)
 
@@ -38,4 +46,47 @@ func Get(db *sql.DB, id int64) (*Folder, error) {
 	}
 
 	return &f, nil
+}
+
+func getSubFolder(db *sql.DB, id int64) ([]Folder, error) {
+	stmt := `select * from "folders" where "parent_id" = $1 and "deleted" = false`
+	rows, err := db.Query(stmt, id)
+	if err != nil {
+		return nil, err
+	}
+
+	f := make([]Folder, 0)
+	for rows.Next() {
+		var folder Folder
+		err := rows.Scan(&folder.ID, &folder.ParentID, &folder.Name, &folder.CreatedAt, &folder.ModifiedAt, &folder.Deleted)
+		if err != nil {
+			continue
+		}
+
+		f = append(f, folder)
+	}
+
+	return f, nil
+}
+
+func GetFolderContent(db *sql.DB, folderID int64) ([]FolderResource, error) {
+	subfolders, err := getSubFolder(db, folderID)
+	if err != nil {
+		return nil, err
+	}
+
+	fr := make([]FolderResource, 0, len(subfolders))
+	for _, subfolder := range subfolders {
+		r := FolderResource{
+			ID:         subfolder.ID,
+			Name:       subfolder.Name,
+			Type:       "directory",
+			CreatedAt:  subfolder.CreatedAt,
+			ModifiedAt: subfolder.ModifiedAt,
+		}
+
+		fr = append(fr, r)
+	}
+
+	return fr, nil
 }
