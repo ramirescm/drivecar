598183633fbbb8fa1d0ff33db54f2b7f4b78681e
Ramires <ramirescm@gmail.com>
2024-07-12 21:06:05 -0300

    chore: add get folder

---
+++ internal/folders/get.go
@@ -0,0 +1,41 @@
+package folders
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func (h *handler) Get(rw http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	u, err := Get(h.db, int64(id))
+	if err != nil {
+		// todo validate if record is not found
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(u)
+}
+
+func Get(db *sql.DB, id int64) (*Folder, error) {
+	stmt := `select * from "folders" where id = $1`
+	row := db.QueryRow(stmt, id)
+
+	var f Folder
+	err := row.Scan(&f.ID, &f.ParentID, &f.Name, &f.CreatedAt, &f.ModifiedAt, &f.Deleted)
+	if err != nil {
+		return nil, err
+	}
+
+	return &f, nil
+}
